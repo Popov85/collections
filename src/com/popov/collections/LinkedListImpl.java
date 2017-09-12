90c73b9ee9dfd80135ba77d7d6490e15c664c1e0
Popov85 <andrey.popov85@gmail.com>
2017-09-12 17:16:44 +0300

    LinkedList: basic methods

--- src/com/popov/collections/LinkedListImpl.java
+++ src/com/popov/collections/LinkedListImpl.java
@@ -1,6 +1,7 @@
 package com.popov.collections;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 /**
  * Created by Andrey on 04.09.2017.
@@ -48,8 +49,31 @@ public class LinkedListImpl<E> implements List<E> {
         }
 
         @Override
-        public void add(int index, E element) {
-
+        public void add(int index, E e) {
+                if (index<=0) {
+                        // add at the beginning
+                        Node<E> n = new Node<E>(e, null, first);
+                        first.prev = n;
+                        first=n;
+                        size++;
+                        return;
+                }
+                // add to the end
+                if (index>=size-1) {
+                        add(e);
+                        return;
+                }
+                // somewhere in the middle
+                Node<E> n = first;
+                int i = 0;
+                while (index!=i) {
+                        n=n.next;
+                        i++;
+                }
+                Node<E> node = new Node<E>(e, n.prev, n);
+                n.prev.next=node;
+                n.prev=node;
+                size++;
         }
 
         @Override
@@ -79,17 +103,58 @@ public class LinkedListImpl<E> implements List<E> {
 
         @Override
         public int indexOf(Object o) {
-                return 0;
+                Node<E> n = first;
+                int i = 0;
+                while (n!=null) {
+                        if (n.item.equals(o)) return i;
+                        n=n.next;
+                        i++;
+                }
+               /* for (Node<E> x = first; x != null; x = x.next) {
+                        if (x.item.equals(o)) return i;
+                        i++;
+                }*/
+                return -1;
         }
 
         @Override
         public E remove(int index) {
-                return null;
+                if (size==0) throw new IllegalStateException("The list is empty, nothing to delete!");
+                // remove first
+                if (index<=0) {
+                        first.next.prev=null;
+                        Node<E> removed = first;
+                        first=first.next;
+                        size--;
+                        return removed.item;
+                }
+                //remove last
+                if (index>=size-1) {
+                        last.prev.next=null;
+                        Node<E> removed = last;
+                        last=last.prev;
+                        size--;
+                        return removed.item;
+
+                }
+                Node<E> n = first;
+                int i = 0;
+                while (index!=i) {
+                        n=n.next;
+                        i++;
+                }
+                n.prev.next=n.next;
+                n.next.prev=n.prev;
+                size--;
+                return n.item;
         }
 
         @Override
         public boolean remove(Object o) {
-                return false;
+                int index = indexOf(o);
+                if (index==-1) return false;
+                remove(index);
+                return true;
         }
 
         @Override
@@ -105,11 +170,10 @@ public class LinkedListImpl<E> implements List<E> {
         @Override
         public String toString() {
                 StringBuilder sb = new StringBuilder();
-                Node n = this.first;
-                sb.append(n.item+"/ ");
-                while (n.hasNext()) {
-                        n=n.next;
+                Node<E> n = first;
+                while (n!=null) {
                         sb.append(n.item+"/ ");
+                        n=n.next;
                 }
                 return "LinkedListImpl{" +
                         "size=" + size +
--- src/com/popov/collections/ListTest.java
+++ src/com/popov/collections/ListTest.java
@@ -9,6 +9,11 @@ public class ListTest {
                 l.add("Andrii");
                 l.add("Eugen");
                 l.add("Valentine");
+                //l.add(2, "XXX");
                 System.out.println(l.toString());
+                System.out.println(l.indexOf("Valentine"));
+
+               /* System.out.println(l.remove(0));
+                System.out.println(l.toString());*/
         }
 }
